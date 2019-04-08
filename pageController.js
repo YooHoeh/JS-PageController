2cfbcc7720cd26a1f960394ef39ec3f39d63159a
YooHoeh <zako2810@sina.com>
2019-04-08 12:11:12 +0800

    fixed some bug

--- pageController.js
+++ pageController.js
@@ -1,22 +1,26 @@
-
 /**
- * 
- * @param {列表渲染目标节点} listTarget 
- * @param {页面控制器目标节点} navTarget 
- * @param {列表项渲染函数} renderFunc 
- * @param {每页项数} pageSize 
- * @param {列表数据源} dataSourse 
+ *
+ * @param {列表渲染目标节点} listTarget
+ * @param {页面控制器目标节点} navTarget
+ * @param {列表项渲染函数} renderFunc
+ * @param {每页项数} pageSize
+ * @param {列表数据源} dataSourse
  */
-function renderPageNav(listTarget, navTarget, renderFunc, pageSize, dataSourse) {
-
+function PageController(
+  listTarget,
+  navTarget,
+  renderFunc,
+  pageSize,
+  dataSourse
+) {
   // 总页数
   const pageLength = Math.ceil(dataSourse.length / pageSize);
 
   //页面控制器
-  const pageController = {
+  const controller = {
     cur: 0,
-    toPage: function (index) {
-      listTarget.innerHTML = '';
+    toPage: function(index) {
+      listTarget.innerHTML = "";
       for (let i = (index - 1) * pageSize; i < index * pageSize; i++) {
         if (dataSourse[i]) {
           listTarget.appendChild(renderFunc(dataSourse[i]));
@@ -25,70 +29,76 @@ function renderPageNav(listTarget, navTarget, renderFunc, pageSize, dataSourse) 
         }
       }
       this.cur = index;
-      console.log(this.cur)
+      console.log(this.cur);
     },
-    next: function () { this.toPage(this.cur + 1) },
-    prev: function () { this.toPage(this.cur - 1) },
-    start: function () { this.toPage(1) },
-    end: function () { this.toPage(pageLength) }
-  }
+    next: function() {
+      this.toPage(this.cur + 1);
+    },
+    prev: function() {
+      this.toPage(this.cur - 1);
+    },
+    start: function() {
+      this.toPage(1);
+    },
+    end: function() {
+      this.toPage(pageLength);
+    }
+  };
   // 页码按钮渲染
   const pageBTN = index => {
-    const btn = document.createElement('b');
+    const btn = document.createElement("b");
     btn.innerHTML = index;
-    btn.onclick = () => pageController.toPage(index);
-    return btn
+    btn.onclick = () => controller.toPage(index);
+    return btn;
   };
 
   // 第一页按钮
-  const startBTN = document.createElement('b');
-  startBTN.innerHTML = '《';
+  const startBTN = document.createElement("b");
+  startBTN.innerHTML = "《";
   startBTN.onclick = () => {
-    pageController.start()
-  }
-  navTarget.appendChild(startBTN)
+    controller.start();
+  };
+  navTarget.appendChild(startBTN);
 
   // 上一页按钮
-  const prevBTN = document.createElement('b');
-  prevBTN.innerHTML = '<';
+  const prevBTN = document.createElement("b");
+  prevBTN.innerHTML = "<";
   prevBTN.onclick = () => {
-    if (pageController.cur != 1)
-      pageController.prev()
-  }
-  navTarget.appendChild(prevBTN)
+    if (controller.cur != 1) controller.prev();
+  };
+  navTarget.appendChild(prevBTN);
 
-  // 页面跳转按钮 
-  const page = document.createElement('span');
+  // 页面跳转按钮
+  const page = document.createElement("span");
   for (let i = 1; i <= pageLength; i++) {
-    page.appendChild(pageBTN(i))
+    page.appendChild(pageBTN(i));
   }
 
-  navTarget.appendChild(page)
+  navTarget.appendChild(page);
 
   // 下一页按钮
-  const nextBTN = document.createElement('b');
-  nextBTN.innerHTML = '>';
+  const nextBTN = document.createElement("b");
+  nextBTN.innerHTML = ">";
   nextBTN.onclick = () => {
-    if (pageController.cur != pageLength) {
-      pageController.next()
+    if (controller.cur != pageLength) {
+      controller.next();
     }
-  }
-  navTarget.appendChild(nextBTN)
+  };
+  navTarget.appendChild(nextBTN);
   // 尾页按钮
-  const endBTN = document.createElement('b');
-  endBTN.innerHTML = '》';
+  const endBTN = document.createElement("b");
+  endBTN.innerHTML = "》";
   endBTN.onclick = () => {
-    if (pageController.cur != pageLength) {
-      pageController.end()
+    if (controller.cur != pageLength) {
+      controller.end();
     }
-  }
-  navTarget.appendChild(endBTN)
-
+  };
+  navTarget.appendChild(endBTN);
 }
 // 列表渲染目标节点
 const listTarget = document.querySelector(".root");
 // 导航栏渲染目标节点
-const navTarget = document.querySelector("#nav");
+const navTarget = document.querySelector(".nav");
 
 //渲染单个Item
 function renderItem(item) {
@@ -134,4 +144,4 @@ var data = [
   }
 ];
 
-renderPageNav(listTarget, navTarget, renderItem, 3, data);
+PageController(listTarget, navTarget, renderItem, 3, data);
